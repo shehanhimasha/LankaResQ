f8a1ffb800bc8ee8261b97056d50695fa99bfe02
shehanhimasha <ssilvassilva96@gmail.com>
2026-05-28 17:46:45 +0530

    Fix URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,35 +1,43 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://disastermgtpro.runasp.net',
+    baseURL:
+        import.meta.env.VITE_API_BASE_URL ||
+        'https://disastermgtpro.runasp.net',
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
     },
 });
 
-// Attach Authorization header if token is available in localStorage
-api.interceptors.request.use((config) => {
-    try {
-        const stored = localStorage.getItem('user');
-        if (stored) {
-            const user = JSON.parse(stored);
-            const token = user.token || user.accessToken || localStorage.getItem('authToken');
-            if (token) {
-                config.headers = config.headers || {};
-                config.headers['Authorization'] = `Bearer ${token}`;
+api.interceptors.request.use(
+    (config) => {
+        try {
+            const stored = localStorage.getItem('user');
+
+            if (stored) {
+                const user = JSON.parse(stored);
+
+                const token =
+                    user.token ||
+                    user.accessToken ||
+                    localStorage.getItem('authToken');
+
+                if (token) {
+                    config.headers.Authorization = `Bearer ${token}`;
+                }
+            } else {
+                const token = localStorage.getItem('authToken');
+
+                if (token) {
+                    config.headers.Authorization = `Bearer ${token}`;
+                }
             }
-        } else {
-            const token = localStorage.getItem('authToken');
-            if (token) {
-                config.headers = config.headers || {};
-                config.headers['Authorization'] = `Bearer ${token}`;
-            }
-        }
-    } catch (e) {
-        // ignore
-    }
-    return config;
-}, (error) => Promise.reject(error));
+        } catch (e) {}
+
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 export default api;
