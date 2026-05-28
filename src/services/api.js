77424242cc494836271cc5610916fe76454e9969
shehanhimasha <ssilvassilva96@gmail.com>
2026-05-28 17:53:00 +0530

    Fix URL

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL:
         import.meta.env.VITE_API_BASE_URL ||
-        'https://disastermgtpro.runasp.net',
+        'http://disastermgtpro.runasp.net',
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
